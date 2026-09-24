ab8a6f9901bb828a7dc1fe08e82c001338e19d7f
Saul Silva <jh168821@gmail.com>
2026-09-24 16:18:56 -0600

    fix: hardcode real D1 database ID in framework script

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "21e47c7e-963a-4cba-9b2d-178c8f8e58c4";
 
 const { d1, r2 } = hostingConfig;
 
